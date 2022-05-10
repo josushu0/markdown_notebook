fd2ae6aa2246529d493a29cbfdde5e3292cd433b
Josué Israel Martell Castro <josue.martell17@gmail.com>
2022-05-09 21:15:00 -0500

    feat: :sparkles: Implemented basic Markdown editing and preview function

--- lib/pages/editor.dart
+++ lib/pages/editor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class EditorPage extends StatefulWidget {
   const EditorPage({Key? key}) : super(key: key);
@@ -9,25 +10,54 @@ class EditorPage extends StatefulWidget {
 
 class _EditorPageState extends State<EditorPage>
     with SingleTickerProviderStateMixin {
+  String text = "";
+  late TabController tabController;
+  late TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(
+      length: 2,
+      vsync: this,
+    );
+    textEditingController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Editor"),
-        bottom: TabBar(
-            controller: TabController(
-              length: 2,
-              vsync: this,
-            ),
-            tabs: const [
-              Tab(
-                child: Icon(Icons.edit),
-              ),
-              Tab(
-                child: Icon(Icons.visibility),
-              )
-            ]),
+        bottom: TabBar(controller: tabController, tabs: const [
+          Tab(
+            child: Icon(Icons.edit),
+          ),
+          Tab(
+            child: Icon(Icons.visibility),
+          )
+        ]),
       ),
+      body: TabBarView(controller: tabController, children: [
+        Container(
+          margin: const EdgeInsets.all(20),
+          child: TextField(
+            expands: true,
+            maxLines: null,
+            keyboardType: TextInputType.multiline,
+            controller: textEditingController,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+            ),
+            onChanged: (String text) {
+              setState(() {
+                this.text = text;
+              });
+            },
+          ),
+        ),
+        Markdown(data: text),
+      ]),
     );
   }
 }
